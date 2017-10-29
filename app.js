db140b05e1f127159591939a69054ee41b2b48fd
Martin Sommer <martin.sommer@staffbase.com>
2017-10-29 12:27:50 +0100

    :wrench: added cors headers

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
+
 
 var index = require('./routes/index');
 var place = require('./routes/place');
@@ -32,6 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
 app.use('/api/place', place);
